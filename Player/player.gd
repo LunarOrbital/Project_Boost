7dcdc100d5144b62e7c478d0a4f4a46a71f3ff6b
LunarOrbital <254162809+LunarOrbital@users.noreply.github.com>
2026-01-12 12:41:39 -0600

    final

--- Player/player.gd
+++ Player/player.gd
@@ -4,6 +4,11 @@ class_name Player
 @export_range(750, 2500) var engineForce := 1400.0
 @export var SASens := 500.0
 var trans := false
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var main_thrust: AudioStreamPlayer = $MainThrust
+
+
 func _ready() -> void:
 	print("ahh")
 
@@ -11,6 +16,10 @@ func _process(delta: float) -> void:
 	if !trans:
 		if (Input.is_action_pressed("boost")):
 			apply_central_force(basis.y * delta * engineForce)
+			if !(main_thrust.is_playing()):
+				main_thrust.play()
+			else:
+				main_thrust.stop()
 		if (Input.is_action_pressed("r_left")):
 			apply_torque(Vector3(0,0,delta * SASens))
 		if (Input.is_action_pressed("r_right")):
@@ -33,8 +42,9 @@ func _crash_sequence() -> void:
 	await get_tree().create_timer(1).timeout
 	print("yes Rico, Kaboom.")
 	get_tree().reload_current_scene.call_deferred()
-	
+	explosion_audio.play()
 func _complete_level(next_level_file) -> void:
+	success_audio.play()
 	trans = true
 	print("u winned")
 	await get_tree().create_timer(1).timeout
